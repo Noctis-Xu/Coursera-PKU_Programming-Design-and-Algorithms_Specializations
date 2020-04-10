ec6b8b1de13aa04d5932843edfd3869d54309d8c
Noctis <xuboyang1j@126.com>
2020-04-10 14:02:55 +0800

    C++_3_4 魔兽世界备战 temp_sync

---
+++ 北京大学程序设计与算法专项课程_Coursera/C++_3_4 魔兽世界之一：备战.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Warrior {
+private:
+	int No;
+	int strength;
+	int attack;
+
+};
+class Headquarter {
+	static int HP;
+	//friend Warrior;
+};
+
+
+int main() {
+
+	return 0;
+}
